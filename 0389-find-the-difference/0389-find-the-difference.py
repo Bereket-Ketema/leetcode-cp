c498f807ffc661e45e9017beb5ebbd0ce2f7d02c
Bereket Ketema <bereketketema866@gmail.com>
2026-01-24 04:28:04 -0300

    Time: 3 ms (56.13%), Space: 19.3 MB (28.21%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        g = list(t)
-        print(g)
-        for i in s:
-            if i in g:
-                g.remove(i)
-        return g[0]
+        store = {}
+        for i in range(len(t)):
+            if t[i] in store:
+                store[t[i]] +=1
+            else:
+                store[t[i]] = 1
+        for i in range(len(s)):
+            if s[i] in store:
+                store[s[i]] -=1
+            else:
+                store[s[i]] = -1
+        for key, value in store.items():
+            if value == 1:
+                return key
