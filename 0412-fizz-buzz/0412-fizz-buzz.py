a82ece6f40dd3a7c320220a497ee43fdf8eee971
Bereket Ketema <bereketketema866@gmail.com>
2026-01-27 21:55:20 -0300

    Time: 1 ms (36.44%), Space: 19.6 MB (21.89%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> list[str]:
+        store = []
+        for i in range(1,n+1):
+            if i % 3 ==0 and i % 5 == 0:
+                store.append("FizzBuzz")
+            elif i % 3 == 0:
+                store.append("Fizz")
+            elif i % 5 == 0:
+                store.append("Buzz")
+            else:
+                store.append(str(i))
+        return store
