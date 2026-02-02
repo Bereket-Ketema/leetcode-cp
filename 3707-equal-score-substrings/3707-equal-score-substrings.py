394f1f0e3d2f4d6b8c035680562955e3bc49bbc0
Bereket Ketema <bereketketema866@gmail.com>
2026-02-02 16:54:32 +0300

    Time: 0 ms (100%), Space: 19.4 MB (14.03%) - LeetHub

--- 3707-equal-score-substrings/3707-equal-score-substrings.py
+++ 3707-equal-score-substrings/3707-equal-score-substrings.py
@@ -1,12 +1,12 @@
 class Solution:
     def scoreBalance(self, s: str) -> bool:
-        left = 0
+        left = ord(s[0]) - 96
         right = 0
-        for i in range(len(s)):
-            left += ord(s[i]) - 96
-            for j in range(i+1,len(s)):
-                right += ord(s[j]) - 96
+        for i in range(1,len(s)):
+            right += ord(s[i]) - 96
+        for j in range(1,len(s)):
             if left == right:
                 return True
-            right = 0
+            right = right -( ord(s[j]) - 96 )
+            left += ord(s[j]) - 96
         return False
