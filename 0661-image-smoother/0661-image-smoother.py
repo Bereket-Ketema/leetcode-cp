1af6c85c8f4dcbed036871cd15bb10b01b84c15d
Bereket Ketema <bereketketema866@gmail.com>
2026-02-13 14:40:49 +0300

    Time: 141 ms (64.8%), Space: 20.2 MB (41.57%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,19 @@
+class Solution:
+    def imageSmoother(self, img):
+        rows = len(img)
+        cols = len(img[0])
+        res = [[0]*cols for _ in range(rows)]
+        
+        for i in range(rows):
+            for j in range(cols):
+                total = 0
+                count = 0
+                for x in range(i-1, i+2):
+                    for y in range(j-1, j+2):
+                        if 0 <= x < rows and 0 <= y < cols:
+                            total += img[x][y]
+                            count += 1
+                
+                res[i][j] = total // count
+        
+        return res
