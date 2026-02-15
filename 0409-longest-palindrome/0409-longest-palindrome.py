59dd93b52243cd870e79f44cdbc1b8133ba1c095
Bereket Ketema <bereketketema866@gmail.com>
2026-02-15 17:24:35 +0300

    Time: 4 ms (15.16%), Space: 19.4 MB (47.96%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freq = Counter(s)
+        length = 0
+        has_odd = False
+
+        for v in freq.values():
+            length += v // 2 * 2   # take only even part
+            if v % 2 == 1:
+                has_odd = True
+
+        if has_odd:
+            length += 1
+
+        return length
