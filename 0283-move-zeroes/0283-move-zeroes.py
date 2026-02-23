bb309f95048426904b0b493ade909f73669bdb33
Bereket Ketema <bereketketema866@gmail.com>
2026-02-23 19:03:24 +0300

    Time: 7 ms (43.1%), Space: 20.6 MB (51.6%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        holder, seeker = 0, 0
+        while seeker < len(nums):
+            if nums[seeker] != 0:
+                nums[seeker], nums[holder] = nums[holder], nums[seeker]
+                holder += 1
+            seeker += 1
+        return nums
