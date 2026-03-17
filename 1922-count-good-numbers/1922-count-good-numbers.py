571ef8f5375a0cea26749d1a2ff4b5dc5ec88a53
Bereket Ketema <bereketketema866@gmail.com>
2026-03-17 18:07:11 +0300

    Time: 0 ms (100%), Space: 19.5 MB (21.48%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -9,10 +9,10 @@ class Solution:
             while n > 1:
                 if n % 2:
                     res = (res * x) % MOD
-                n = n // 2
+                n //= 2
                 x = (x * x) % MOD
             return res * x
-        even = ceil(n / 2)
+        even = (n + 1) // 2
         odd = n // 2
 
         return (pow(5, even) * pow(4, odd)) % MOD 
