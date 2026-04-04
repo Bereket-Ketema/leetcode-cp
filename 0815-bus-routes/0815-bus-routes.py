4788d88318da50aac7f10db21ffe6dff93556e8f
Bereket Ketema <bereketketema866@gmail.com>
2026-04-04 16:58:05 +0300

    Time: 81 ms (66.71%), Space: 45.7 MB (71.17%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.py
@@ -0,0 +1,34 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def numBusesToDestination(self, routes, source, target):
+        if source == target:
+            return 0
+
+        stop_to_buses = defaultdict(list)
+        for i, route in enumerate(routes):
+            for stop in route:
+                stop_to_buses[stop].append(i)
+        
+        visited_stops = set([source])
+        visited_buses = set()
+        queue = deque([(source, 0)])
+        
+        while queue:
+            stop, buses = queue.popleft()
+            
+            for bus in stop_to_buses[stop]:
+                if bus in visited_buses:
+                    continue
+                
+                visited_buses.add(bus)
+                
+                for next_stop in routes[bus]:
+                    if next_stop == target:
+                        return buses + 1
+                    
+                    if next_stop not in visited_stops:
+                        visited_stops.add(next_stop)
+                        queue.append((next_stop, buses + 1))
+        
+        return -1
