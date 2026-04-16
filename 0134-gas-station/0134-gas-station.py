44c1c17111a2e7217cfd413b7f9a24b437f80b32
Bereket Ketema <bereketketema866@gmail.com>
2026-04-16 21:31:26 +0300

    Time: 36 ms (23.18%), Space: 26 MB (40.39%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        total = curr = start = 0
+        
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            curr += gas[i] - cost[i]
+            
+            if curr < 0:
+                start = i + 1
+                curr = 0
+        
+        return start if total >= 0 else -1
