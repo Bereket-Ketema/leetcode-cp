9f93714235f2967f62730cd000ca26767a6d33d5
Bereket Ketema <bereketketema866@gmail.com>
2026-04-18 18:06:49 +0300

    Time: 626 ms (98.17%), Space: 55.4 MB (86.41%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,28 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = {}
+
+    def addWord(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node:
+                node[ch] = {}
+            node = node[ch]
+        node['#'] = True
+
+    def search(self, word):
+        def dfs(node, i):
+            if i == len(word):
+                return '#' in node
+            ch = word[i]
+            if ch == '.':
+                for child in node:
+                    if child != '#' and dfs(node[child], i + 1):
+                        return True
+                return False
+            if ch not in node:
+                return False
+            return dfs(node[ch], i + 1)
+        
+        return dfs(self.root, 0)
