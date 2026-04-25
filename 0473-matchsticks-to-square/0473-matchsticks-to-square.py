c0fb9051d542dc2f6008e522956de0f8d69c48e5
Bereket Ketema <bereketketema866@gmail.com>
2026-04-25 03:46:28 +0300

    Time: 3930 ms (12.93%), Space: 19.4 MB (58.01%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -0,0 +1,22 @@
+class Solution:
+    def makesquare(self, matchsticks):
+        if not matchsticks or sum(matchsticks) % 4 != 0:
+            return False
+        
+        side = sum(matchsticks) // 4
+        matchsticks.sort(reverse=True)
+        sides = [0] * 4
+        
+        def backtrack(i):
+            if i == len(matchsticks):
+                return True
+            
+            for j in range(4):
+                if sides[j] + matchsticks[i] <= side:
+                    sides[j] += matchsticks[i]
+                    if backtrack(i + 1):
+                        return True
+                    sides[j] -= matchsticks[i]
+            return False
+        
+        return backtrack(0)
