260a39c4b328e3a6752a84b30184578a95bf6e39
Bereket Ketema <bereketketema866@gmail.com>
2026-04-27 08:09:28 +0300

    Time: 3 ms (37.26%), Space: 20 MB (77.78%) - LeetHub

---
+++ 0667-beautiful-arrangement-ii/0667-beautiful-arrangement-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def constructArray(self, n, k):
+        res = []
+        left, right = 1, n
+        
+        while left <= right:
+            if k > 1:
+                if k % 2:
+                    res.append(left)
+                    left += 1
+                else:
+                    res.append(right)
+                    right -= 1
+                k -= 1
+            else:
+                res.append(left)
+                left += 1
+        
+        return res
