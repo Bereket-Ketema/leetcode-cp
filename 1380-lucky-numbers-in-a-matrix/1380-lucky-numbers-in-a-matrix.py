cc03bec94f0f0bc243fe0fc12e8d427e9daaf635
Bereket Ketema <bereketketema866@gmail.com>
2026-04-30 10:56:05 +0300

    Time: 0 ms (100%), Space: 19.5 MB (73.37%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def luckyNumbers(self, matrix):
+        row_min = [min(row) for row in matrix]
+        col_max = [max(col) for col in zip(*matrix)]
+        
+        res = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == row_min[i] == col_max[j]:
+                    res.append(matrix[i][j])
+        
+        return res
