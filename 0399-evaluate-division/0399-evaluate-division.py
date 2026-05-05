3da3b0c728e542764e7efdb011525fa19c39dbb2
Bereket Ketema <bereketketema866@gmail.com>
2026-05-05 21:02:43 +0300

    Time: 0 ms (100%), Space: 19.5 MB (74.71%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def calcEquation(self, equations, values, queries):
+        graph = defaultdict(list)
+
+        for (a, b), val in zip(equations, values):
+            graph[a].append((b, val))
+            graph[b].append((a, 1 / val))
+
+        def dfs(src, dst, visited):
+            if src not in graph:
+                return -1.0
+            if src == dst:
+                return 1.0
+
+            visited.add(src)
+
+            for nei, val in graph[src]:
+                if nei not in visited:
+                    res = dfs(nei, dst, visited)
+                    if res != -1:
+                        return val * res
+
+            return -1.0
+
+        ans = []
+
+        for a, b in queries:
+            ans.append(dfs(a, b, set()))
+
+        return ans
