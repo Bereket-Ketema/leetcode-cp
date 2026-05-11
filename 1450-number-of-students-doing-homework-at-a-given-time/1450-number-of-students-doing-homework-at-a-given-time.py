1a95ed09e1f5a43686c48b93591f12c76d6fb58e
Bereket Ketema <bereketketema866@gmail.com>
2026-05-11 10:45:04 +0300

    Time: 0 ms (100%), Space: 19.2 MB (63.64%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,9 @@
+class Solution:
+    def busyStudent(self, startTime, endTime, queryTime):
+        ans = 0
+        
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                ans += 1
+        
+        return ans
