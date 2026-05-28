08164bee15ad6e62a80bfc434d9988116c031679
Bereket Ketema <bereketketema866@gmail.com>
2026-05-28 09:42:42 +0300

    Time: 51 ms (77.23%), Space: 21 MB (58.24%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root):
+        ans = []
+        
+        def dfs(node):
+            if not node:
+                return
+            
+            ans.append(node.val)
+            
+            for child in node.children:
+                dfs(child)
+        
+        dfs(root)
+        return ans
