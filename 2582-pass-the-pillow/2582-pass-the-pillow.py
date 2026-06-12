b946104597790596ced72b882ffcc9eaedc9eebc
Bereket Ketema <bereketketema866@gmail.com>
2026-06-13 01:24:59 +0300

    Time: 0 ms (100%), Space: 19.3 MB (59.61%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -0,0 +1,10 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        cycle = n - 1
+        
+        pos = time % (2 * cycle)
+        
+        if pos <= cycle:
+            return pos + 1
+        
+        return n - (pos - cycle)
