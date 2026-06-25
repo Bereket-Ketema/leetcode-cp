c8dd88697b88426cddf1db0870e9b2d3eb2f124e
Bereket Ketema <bereketketema866@gmail.com>
2026-06-25 23:53:46 +0300

    Time: 6 ms (92.88%), Space: 19.7 MB (88.82%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -0,0 +1,15 @@
+class ParkingSystem:
+
+    def __init__(self, big, medium, small):
+        self.slots = [0, big, medium, small]
+
+    def addCar(self, carType):
+        if self.slots[carType] > 0:
+            self.slots[carType] -= 1
+            return True
+        return False
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
