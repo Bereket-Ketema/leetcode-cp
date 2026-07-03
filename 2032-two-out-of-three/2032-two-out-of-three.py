fbc95c637d5601d2ce03dd03ac6e26c605a3f147
Bereket Ketema <bereketketema866@gmail.com>
2026-07-03 09:34:33 +0300

    Time: 0 ms (100%), Space: 19.4 MB (36.08%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoOutOfThree(self, nums1, nums2, nums3):
+        s1 = set(nums1)
+        s2 = set(nums2)
+        s3 = set(nums3)
+
+        return list((s1 & s2) | (s1 & s3) | (s2 & s3))
