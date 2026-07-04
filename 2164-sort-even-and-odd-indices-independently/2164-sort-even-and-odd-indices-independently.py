c48beac902e95db37342dd526826ed05d81145c6
Bereket Ketema <bereketketema866@gmail.com>
2026-07-04 05:22:40 +0300

    Time: 0 ms (100%), Space: 19.4 MB (26.7%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortEvenOdd(self, nums):
+        even = sorted(nums[::2])
+        odd = sorted(nums[1::2], reverse=True)
+
+        e = o = 0
+
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                nums[i] = even[e]
+                e += 1
+            else:
+                nums[i] = odd[o]
+                o += 1
+
+        return nums
