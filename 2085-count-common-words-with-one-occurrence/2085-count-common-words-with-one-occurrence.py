c8f2a4d33d7cf712161a1f57dbfc07d8241a5932
Bereket Ketema <bereketketema866@gmail.com>
2026-07-04 04:00:31 +0300

    Time: 4 ms (43.22%), Space: 19.6 MB (76.55%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution:
+    def countWords(self, words1, words2):
+        c1 = Counter(words1)
+        c2 = Counter(words2)
+
+        ans = 0
+
+        for word in c1:
+            if c1[word] == 1 and c2[word] == 1:
+                ans += 1
+
+        return ans
